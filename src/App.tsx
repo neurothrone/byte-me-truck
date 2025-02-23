67017aea51f07102fc77dc87093170ddcc2b5a87
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-02-23 06:22:40 +0100

    feat: refactor App component to use arrow function syntax

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
 import AppRouter from "./routes/AppRouter.tsx";
 
-function App() {
+const App = () => {
   return (
     <Provider store={store}>
       <AppRouter/>
